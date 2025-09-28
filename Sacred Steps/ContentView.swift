961c9cf8f9988fb46614ca8c2afb02c213922e9c
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-09-28 15:44:07 -0500

    Initial Commit

---
+++ Sacred Steps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sacred Steps
+//
+//  Created by Justin Hold on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sacred Steps/Sacred_StepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sacred_StepsApp.swift
+//  Sacred Steps
+//
+//  Created by Justin Hold on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Sacred_StepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
